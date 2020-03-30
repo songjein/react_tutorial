9ded308172e05c84c6c025f131f9683d2b1afec7
songjein <jeinsong100@gmail.com>
2020-03-30 06:19:16 +0000

    state

--- src/App.js
+++ src/App.js
@@ -12,29 +12,29 @@ function Food({ name, picture, rating }) {
 }
 
 Food.propTypes = {
-	name: PropTypes.string.isRequired,
-	picture: PropTypes.string.isRequired,
-	rating: PropTypes.number.isRequired,
-}
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 const foodILike = [
   {
     id: 1,
     name: "kimchi",
     image: "https://via.placeholder.com/150/FF0000",
-    rating: 5.0
+    rating: 5.0,
   },
   {
     id: 2,
     name: "samgyep",
     image: "https://via.placeholder.com/150/0000FF",
-    rating: 5.5
+    rating: 5.5,
   },
   {
     id: 3,
     name: "jeinsong",
     image: "https://via.placeholder.com/150/00FF00",
-    rating: 5.1
+    rating: 5.1,
   },
 ];
 
@@ -42,15 +42,55 @@ function renderFood(dish) {
   return <Food name={dish.name} picture={dish.image} />;
 }
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-      {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-      ))}
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello
+//       {foodILike.map((dish) => (
+//         <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+//       ))}
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
+  state = {
+    count: 0,
+    isLoading: true,
+  };
+  add = () => {
+    // this.setState({ count: this.state.count + 1 });
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    //this.setState({ count: this.state.count - 1 });
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  render() {
+    const { isLoading } = this.state;
+    console.log("component is rendering");
+    return (
+      <div>
+        <div>{isLoading ? "Loadding..." : "We are ready"}</div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
